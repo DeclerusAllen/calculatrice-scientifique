314431641292eda342dcd4bd718dd0dbedbfae74
DeclerusAllen <declerusallen20@gmail.com>
2026-02-03 10:50:38 -0500

    fix validation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculator Scienst Project 
+
+
+## Goal
+help the user to sum 2 numbers
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 def main():
-    print("Hello from calculatrice-scientifique!")
+    def sumnumber(number1, number2):
+        return number1 + number2
 
+    def get_valid_number(prompt):
+        while True:
+            try:
+                return float(input(prompt))
+            except ValueError:
+                print("invalid input, please enter numeric values.")
+
+    number1 = get_valid_number("enter the first number: ")
+    number2 = get_valid_number("enter the second number: ")
+
+    print("the result is: ", sumnumber(number1, number2))
 
 if __name__ == "__main__":
     main()
